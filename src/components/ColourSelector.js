6280b97ee628b03084321991a972d64a3a5b30a1
sshubham681 <sshubham681@gmail.com>
2021-09-09 16:10:55 +0530

    done with assignment

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -3,9 +3,10 @@ import React from 'react';
 const ColourSelector = (props) => {
   const { config, selectNextBackground } = props
   const { background } = config;
+
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
+    <button className={config.className} onClick={() => selectNextBackground({ background: background })}>
+      {config.label}
     </button>
   )
 }
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,21 @@
+import React, { useState } from 'react'
+import "../styles/Child.css";
 
+function Selection({ applyColor }) {
+  const [color, setColor] = useState({ background: '' })
+
+  function handleClick() {
+    applyColor(setColor);
+    console.log("done");
+  }
+
+  return (
+    <div className="fix-box"
+      style={color}
+      onClick={handleClick}>
+      <h2 className="subheading">Selection</h2>
+    </div>
+  )
+}
+
+export default Selection
